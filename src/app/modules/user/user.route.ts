94170a66cdf006988b8fcb319c405cd0d6b565d2
Rahad-Ullah <rahadullah10@gmail.com>
2025-11-02 11:59:20 +0600

    refact: router file and user routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,33 +1,28 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validation';
 import fileUploadHandler from '../../middlewares/fileUploadHandler';
-import { USER_ROLES } from './user.constant';
 const router = express.Router();
 
-router
-  .route('/profile')
-  .get(auth(USER_ROLES.ADMIN, USER_ROLES.USER), UserController.getUserProfile)
-  .patch(
-    auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.USER),
-    fileUploadHandler(),
-    (req: Request, res: Response, next: NextFunction) => {
-      if (req.body.data) {
-        req.body = UserValidation.updateUserZodSchema.parse(
-          JSON.parse(req.body.data)
-        );
-      }
-      return UserController.updateProfile(req, res, next);
-    }
-  );
+// create user
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserZodSchema),
+  UserController.createUser
+);
 
-router
-  .route('/')
-  .post(
-    validateRequest(UserValidation.createUserZodSchema),
-    UserController.createUser
-  );
+// update profile
+router.patch(
+  '/profile',
+  auth(),
+  fileUploadHandler(),
+  validateRequest(UserValidation.updateUserZodSchema),
+  UserController.updateProfile
+);
+
+// get profile
+router.get('/profile', auth(), UserController.getUserProfile);
 
 export const UserRoutes = router;
